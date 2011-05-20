2bab627b06a96c18efdbd15481ec7166ce356bd3
Surendra Singhi <ssinghi@kreeti.com>
2011-05-20 23:51:39 +0530

    task to add memcached config file

--- lib/acts_as_cached/railtie.rb
+++ lib/acts_as_cached/railtie.rb
@@ -10,7 +10,7 @@ module ActsAsCached
       Object.send :include, ActsAsCached::Mixin
 
       unless File.exists?(config_file = Rails.root.join('config', 'memcached.yml'))
-        error = "No config file found.  Make sure you used `script/plugin install' and have memcached.yml in your config directory."
+        error = "No config file found. If you used plugin version make sure you used `script/plugin install' or `rake memcached:cache_fu_install' if gem version and have memcached.yml in your config directory."
         puts error
         logger.error error
         exit!
